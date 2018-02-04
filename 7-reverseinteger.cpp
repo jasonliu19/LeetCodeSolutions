009d83ff64d7f4e414b4e2885cc3b1b13d2b2510
Jason Liu <jasonliu1999@gmail.com>
2018-02-03 22:48:06 -0500

    Added solution 7

---
+++ 7-reverseinteger.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int reverse(int x) {
+        
+        int reversed = 0;
+        
+        
+        while(x!=0){
+            
+            int temp = reversed*10;
+            if((temp)/10 != reversed)
+                return 0;
+            reversed= temp + x%10;
+            x/=10;
+        }
+        
+
+        
+        return reversed;
+    }
+};
